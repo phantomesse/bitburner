81097c6102f4c2c773fa88eeb14884ef671fbcde
Lauren Zou <laurenzou@google.com>
2021-12-24 17:02:07 -0500

    Add script for managing hacknet.

---
+++ scripts/manage-hacknet.js
@@ -0,0 +1,21 @@
+/**
+ * Manages buying and upgrading hacknet nodes.
+ *
+ * @param {import('../.').NS } ns
+ */
+export async function main(ns) {
+  while (true) {
+    // Buy new nodes if we can.
+    do {
+      ns.hacknet.purchaseNode();
+    } while (ns.hacknet.purchaseNode() > -1);
+
+    // Upgrade nodes.
+    for (let i = 0; i < ns.hacknet.numNodes(); i++) {
+      ns.hacknet.upgradeCore(i, 1);
+      ns.hacknet.upgradeRam(i, 1);
+      ns.hacknet.upgradeLevel(i, 1);
+      await ns.sleep(1000);
+    }
+  }
+}
