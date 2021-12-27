8c17a81354f0b298d2d0983ef4ca7a93f016abb8
Lauren Zou <laurenzou@google.com>
2021-12-26 23:03:09 -0500

    Delete old low ram servers.

--- scripts/buy-servers.js
+++ scripts/buy-servers.js
@@ -1,3 +1,5 @@
+const DISABLE_LOGGING_FUNCTIONS = ['getServerMaxRam', 'sleep'];
+
 const MIN_POWER = 3; // Min RAM that we want is at least 8GB
 const MAX_POWER = 20; // Max RAM is 2^20
 const HOSTNAME = 'lauren';
@@ -8,17 +10,58 @@ const HOSTNAME = 'lauren';
  * @param {import('..').NS } ns
  */
 export async function main(ns) {
+  DISABLE_LOGGING_FUNCTIONS.forEach(ns.disableLog);
+  const purchasedServerLimit = ns.getPurchasedServerLimit();
+  let lowestRamAcceptable = Math.pow(2, MIN_POWER);
+
   while (true) {
-    // Buy a server.
     for (let power = MAX_POWER; power >= MIN_POWER; power--) {
       const ram = Math.pow(2, power);
+      if (ram < lowestRamAcceptable) continue;
       const cost = ns.getPurchasedServerCost(ram);
       if (ns.getPlayer().money < cost) continue;
+
+      // Delete lowest RAM server if over server limit.
+      if (ns.getPurchasedServers().length === purchasedServerLimit) {
+        const lowestRamServerName = getLowestRamPurchasedServer(ns);
+        const lowestRam = ns.getServerMaxRam(lowestRamServerName);
+        if (ram === lowestRam) continue;
+        ns.killall(lowestRamServerName);
+        if (ns.deleteServer(lowestRamServerName)) {
+          ns.tprint(
+            `deleted server ${lowestRamServerName} which had ${lowestRam}GB RAM`
+          );
+        }
+      }
+
+      // Buy server.
       const server = ns.purchaseServer(HOSTNAME, ram);
-      if (server !== '') ns.tprint(`bought server (${server}) with ${ram} RAM`);
+      if (server !== '') {
+        ns.tprint(`bought server (${server}) with ${ram}GB RAM`);
+
+        // Update lowest RAM acceptable.
+        if (ns.getPurchasedServers().length === purchasedServerLimit) {
+          const lowestRamServerName = getLowestRamPurchasedServer(ns);
+          const lowestRam = ns.getServerMaxRam(lowestRamServerName);
+          if (lowestRam !== lowestRamAcceptable) {
+            lowestRamAcceptable = lowestRam;
+            ns.tprint(
+              `now only buying servers >= ${lowestRamAcceptable}GB RAM`
+            );
+          }
+        }
+      }
     }
 
-    // Wait 5 minutes before buying another server.
-    await ns.sleep(1000 * 60 * 5);
+    // Wait 1 minute before buying another server.
+    await ns.sleep(1000 * 60);
   }
+}
+/** @param {import('..').NS } ns */
+function getLowestRamPurchasedServer(ns) {
+  const purchasedServerNames = ns.getPurchasedServers();
+  purchasedServerNames.sort((serverName1, serverName2) => {
+    return ns.getServerMaxRam(serverName1) - ns.getServerMaxRam(serverName2);
+  });
+  return purchasedServerNames[0];
 }
