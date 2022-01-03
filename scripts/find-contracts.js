37e6efad7ba57f17cfc2e17a5a68a48b6417929d
Lauren Zou <laurenzou@google.com>
2022-01-03 15:20:49 -0500

    Clean up find contracts.

--- scripts/find-contracts.js
+++ scripts/find-contracts.js
@@ -1,28 +1,47 @@
-import { totalWaysToSum } from '/contracts/total-ways-to-sum.js';
 import { arrayJumpingGame } from '/contracts/array-jumping-game.js';
+import { findAllValidMathExpressions } from '/contracts/find-valid-math-solutions.js';
 import { findLargestPrimeFactor } from '/contracts/find-largest-prime-factor.js';
 import { generateIpAddresses } from '/contracts/generate-ip-addresses.js';
+import { mergeOverlappingIntervals } from '/contracts/merge-overlapping-intervals.js';
+import { minimumPathSumInATriangle } from '/contracts/minimum-path-sum-in-a-triangle.js';
+import { sanitizeParenthesesInExpression } from '/contracts/sanitize-parentheses-in-expression.js';
+import { spiralizeMatrix } from '/contracts/spiralize-matrix.js';
+import { subarrayWithMaximumSum } from '/contracts/subarray-with-maximum-sum.js';
+import { totalWaysToSum } from '/contracts/total-ways-to-sum.js';
 import {
   algorithmicStockTraderI,
   algorithmicStockTraderII,
   algorithmicStockTraderIII,
   algorithmicStockTraderIV,
 } from '/contracts/algorithmic-stock-trader.js';
-import { subarrayWithMaximumSum } from '/contracts/subarray-with-maximum-sum.js';
-import { sanitizeParenthesesInExpression } from '/contracts/sanitize-parentheses-in-expression.js';
 import {
   uniquePathsInAGridI,
   uniquePathsInAGridII,
 } from '/contracts/unique-paths-in-a-grid.js';
-import { findAllValidMathExpressions } from '/contracts/find-valid-math-solutions.js';
-import { minimumPathSumInATriangle } from '/contracts/minimum-path-sum-in-a-triangle.js';
-import { mergeOverlappingIntervals } from '/contracts/merge-overlapping-intervals.js';
-import { spiralizeMatrix } from '/contracts/spiralize-matrix.js';
 import {
   getAllServerNames,
   getPath,
   HOME_SERVER_NAME,
 } from '/utils/servers.js';
+
+const CONTRACT_TYPE_TO_SOLVER_FN_MAP = {
+  'Algorithmic Stock Trader I': algorithmicStockTraderI,
+  'Algorithmic Stock Trader II': algorithmicStockTraderII,
+  'Algorithmic Stock Trader III': algorithmicStockTraderIII,
+  'Algorithmic Stock Trader IV': algorithmicStockTraderIV,
+  'Array Jumping Game': arrayJumpingGame,
+  'Find All Valid Math Expressions': findAllValidMathExpressions,
+  'Find Largest Prime Factor': findLargestPrimeFactor,
+  'Generate IP Addresses': generateIpAddresses,
+  'Merge Overlapping Intervals': mergeOverlappingIntervals,
+  'Minimum Path Sum in a Triangle': minimumPathSumInATriangle,
+  'Sanitize Parentheses in Expression': sanitizeParenthesesInExpression,
+  'Spiralize Matrix': spiralizeMatrix,
+  'Subarray with Maximum Sum': subarrayWithMaximumSum,
+  'Total Ways to Sum': totalWaysToSum,
+  'Unique Paths in a Grid I': uniquePathsInAGridI,
+  'Unique Paths in a Grid II': uniquePathsInAGridII,
+};
 
 /**
  * Finds all the contracts across all servers and solves any that we can solve
@@ -75,60 +94,10 @@ class Contract {
    */
   attemptToSolve(ns) {
     const contractType = this._getContractType(ns);
+    if (!(contractType in CONTRACT_TYPE_TO_SOLVER_FN_MAP)) return false;
+
     const input = ns.codingcontract.getData(this.fileName, this.serverName);
-    let answer;
-    switch (contractType) {
-      case 'Total Ways to Sum':
-        answer = totalWaysToSum(input);
-        break;
-      case 'Array Jumping Game':
-        answer = arrayJumpingGame(input);
-        break;
-      case 'Find Largest Prime Factor':
-        answer = findLargestPrimeFactor(input);
-        break;
-      case 'Generate IP Addresses':
-        answer = generateIpAddresses(input);
-        break;
-      case 'Algorithmic Stock Trader I':
-        answer = algorithmicStockTraderI(input);
-        break;
-      case 'Algorithmic Stock Trader II':
-        answer = algorithmicStockTraderII(input);
-        break;
-      case 'Algorithmic Stock Trader III':
-        answer = algorithmicStockTraderIII(input);
-        break;
-      case 'Algorithmic Stock Trader IV':
-        answer = algorithmicStockTraderIV(input);
-        break;
-      case 'Subarray with Maximum Sum':
-        answer = subarrayWithMaximumSum(input);
-        break;
-      case 'Sanitize Parentheses in Expression':
-        answer = sanitizeParenthesesInExpression(input);
-        break;
-      case 'Unique Paths in a Grid I':
-        answer = uniquePathsInAGridI(input);
-        break;
-      case 'Unique Paths in a Grid II':
-        answer = uniquePathsInAGridII(input);
-        break;
-      case 'Find All Valid Math Expressions':
-        answer = findAllValidMathExpressions(input);
-        break;
-      case 'Minimum Path Sum in a Triangle':
-        answer = minimumPathSumInATriangle(input);
-        break;
-      case 'Merge Overlapping Intervals':
-        answer = mergeOverlappingIntervals(input);
-        break;
-      case 'Spiralize Matrix':
-        answer = spiralizeMatrix(input);
-        break;
-      default:
-        return false;
-    }
+    const answer = CONTRACT_TYPE_TO_SOLVER_FN_MAP[contractType](input);
     const response = ns.codingcontract.attempt(
       answer,
       this.fileName,
