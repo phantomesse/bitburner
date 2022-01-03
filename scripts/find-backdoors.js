8b73211c8bded5420c0942914ef9e89f35ceda6c
Lauren Zou <laurenzou@google.com>
2022-01-03 18:23:14 -0500

    Print backdoors to log so that it is easier to copy and paste.

--- scripts/find-backdoors.js
+++ scripts/find-backdoors.js
@@ -4,35 +4,44 @@ import { getAllServerNames, getPath } from '/utils/servers.js';
  * Finds any servers that do not have backdoor installed and can have backdoor
  * installed.
  *
+ * Run this script with `--tail`.
+ *
  * @param {import('..').NS} ns
  */
 export async function main(ns) {
-  const serverNames = getAllServerNames(ns).filter(serverName => {
-    const server = ns.getServer(serverName);
-    return (
-      server.hasAdminRights &&
-      !server.backdoorInstalled &&
-      server.requiredHackingSkill <= ns.getHackingLevel() &&
-      !server.purchasedByPlayer
+  ns.disableLog('sleep');
+
+  while (true) {
+    const serverNames = getAllServerNames(ns).filter(serverName => {
+      const server = ns.getServer(serverName);
+      return (
+        server.hasAdminRights &&
+        !server.backdoorInstalled &&
+        server.requiredHackingSkill <= ns.getHackingLevel() &&
+        !server.purchasedByPlayer
+      );
+    });
+
+    if (serverNames.length === 0) {
+      ns.print('No servers that we can install backdoor available.');
+      return;
+    }
+
+    ns.clearLog();
+    ns.print(
+      '\n' +
+        serverNames
+          .map(
+            serverName =>
+              `${serverName}\n${[
+                'home',
+                ...getPath(ns, serverName).map(path => `connect ${path}`),
+                'backdoor',
+              ].join('; ')}`
+          )
+          .join('\n\n')
     );
-  });
 
-  if (serverNames.length === 0) {
-    ns.tprint('No servers that we can install backdoor available.');
-    return;
+    await ns.sleep(1000);
   }
-
-  ns.tprint(
-    '\n' +
-      serverNames
-        .map(
-          serverName =>
-            `${serverName}\n${[
-              'home',
-              ...getPath(ns, serverName).map(path => `connect ${path}`),
-              'backdoor',
-            ].join('; ')}`
-        )
-        .join('\n\n')
-  );
 }
