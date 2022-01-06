eff62a99e660f8f1fa8a583ffea2397aa9017251
Lauren Zou <laurenzou@google.com>
2022-01-06 17:15:00 -0500

    Add test case for total ways to sum.

---
+++ tests/total-ways-to-sum.test.js
@@ -0,0 +1,5 @@
+import { totalWaysToSum } from '../scripts/contracts/total-ways-to-sum';
+import { runTests } from './utils';
+
+const testCases = [{ input: 23, output: 1254 }];
+runTests(testCases, totalWaysToSum);
