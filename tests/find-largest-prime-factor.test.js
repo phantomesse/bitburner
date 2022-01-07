89b2d0fcaec3a06f723b5b948c18e85c337973eb
Lauren Zou <laurenzou@google.com>
2022-01-07 13:02:36 -0500

    Add test case.

--- tests/find-largest-prime-factor.test.js
+++ tests/find-largest-prime-factor.test.js
@@ -1,5 +1,8 @@
 import { findLargestPrimeFactor } from '../scripts/contracts/find-largest-prime-factor';
 import { runTests } from './utils';
 
-const testCases = [{ input: 785484550, output: 234473 }];
+const testCases = [
+  { input: 785484550, output: 234473 },
+  { input: 777986532, output: 2423 },
+];
 runTests(testCases, findLargestPrimeFactor);
