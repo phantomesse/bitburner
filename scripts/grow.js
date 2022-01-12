a97f50aafb0e28628abf42fbdff817939536db68
Lauren Zou <laurenzou@google.com>
2022-01-12 12:44:15 -0500

    Allow grow, hack, weaken infinite times.

--- scripts/grow.js
+++ scripts/grow.js
@@ -4,18 +4,18 @@
  * If the number of times is passed into the argument, then only runs for that
  * many times. Otherwise, runs infinitely.
  *
- * @example run grow.ns <hostname> <number of times>
+ * @example run grow.js <hostname> <number of times>
  * @param {import('index').NS } ns
  */
 export async function main(ns) {
   const host = ns.args[0];
   const numberOfTimes = ns.args[1];
-  if (typeof host !== 'string' || typeof numberOfTimes !== 'number') {
-    ns.tprint(`usage: run grow.ns <host> <number of times>`);
+  if (typeof host !== 'string') {
+    ns.tprint(`usage: run grow.js <host> <number of times>`);
     return;
   }
   let index = 0;
-  while (isNaN(numberOfTimes) ? true : index++ < numberOfTimes) {
+  while (typeof numberOfTimes !== 'number' ? true : index++ < numberOfTimes) {
     await ns.grow(host, { stock: true });
   }
 }
--- scripts/hack.js
+++ scripts/hack.js
@@ -4,18 +4,18 @@
  * If the number of times is passed into the argument, then only runs for that
  * many times. Otherwise, runs infinitely.
  *
- * @example run hack.ns <hostname> <number of times>
+ * @example run hack.js <hostname> <number of times>
  * @param {import('index').NS } ns
  */
 export async function main(ns) {
   const host = ns.args[0];
   const numberOfTimes = ns.args[1];
-  if (typeof host !== 'string' || typeof numberOfTimes !== 'number') {
-    ns.tprint(`usage: run hack.ns <host> <number of times>`);
+  if (typeof host !== 'string') {
+    ns.tprint(`usage: run hack.js <host> <number of times>`);
     return;
   }
   let index = 0;
-  while (isNaN(numberOfTimes) ? true : index++ < numberOfTimes) {
+  while (typeof numberOfTimes !== 'number' ? true : index++ < numberOfTimes) {
     await ns.hack(host, { stock: true });
   }
 }
