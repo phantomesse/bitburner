eb6f9ae4148f6e8d9cbc63eec7b424a62172835d
Lauren Zou <zou.lauren@gmail.com>
2024-03-04 11:58:30 -0500

    Update

--- scripts/manage-hacknet.js
+++ scripts/manage-hacknet.js
@@ -1,4 +1,12 @@
 import { ONE_SECOND, HOME_HOSTNAME } from 'utils/constants';
+import { formatMoney } from 'utils/format';
+import { printTable } from 'utils/table';
+
+const NODE_NAME_COLUMN = { name: 'Node Name', style: { width: 'max-content' } };
+const PRODUCTION_COLUMN = {
+  name: 'Production',
+  style: { textAlign: 'center', width: 'max-content' },
+};
 
 /**
  * Manages purchasing and upgrading Hacknet nodes.
@@ -6,7 +14,21 @@ import { ONE_SECOND, HOME_HOSTNAME } from 'utils/constants';
  * @param {NS} ns
  */
 export async function main(ns) {
+  ns.disableLog('ALL');
+  ns.atExit(() => ns.closeTail());
+
+  // const augmentations = ns.singularity.getOwnedAugmentations();
+  // ns.singularity.getAugmentationStats().hackn;
+
+  // ns.tprint(
+  //   formatMoney(
+  //     ns,
+  //     ns.formulas.hacknetNodes.hacknetNodeCost(ns.hacknet.numNodes() + 1, 1)
+  //   )
+  // );
+
   while (true) {
+    ns.clearLog();
     const moneyAvailable = ns.getServerMoneyAvailable(HOME_HOSTNAME) / 2;
 
     // Attempt to purchase a new node.
@@ -38,6 +60,7 @@ export async function main(ns) {
       );
     }
 
+    log(ns);
     await ns.sleep(ONE_SECOND);
   }
 }
@@ -54,4 +77,59 @@ function upgrade(canUpgradeFunction, upgradeFunction) {
     canUpgrade = canUpgradeFunction();
     canUpgrade = upgradeFunction();
   } while (canUpgrade);
+}
+
+/**
+ * Logs to tail.
+ *
+ * @param {NS} ns
+ */
+function log(ns) {
+  const nodeCount = ns.hacknet.numNodes();
+  let totalProduction = 0;
+
+  /** @type {import('utils/table').Table} */ const table = { rows: [] };
+  for (let nodeIndex = 0; nodeIndex < nodeCount; nodeIndex++) {
+    const nodeStats = ns.hacknet.getNodeStats(nodeIndex);
+    totalProduction += nodeStats.production;
+
+    /** @type {import('utils/table').Row} */ const row = {
+      cells: [
+        { column: NODE_NAME_COLUMN, content: `hacknet-node-${nodeIndex}` },
+        {
+          column: PRODUCTION_COLUMN,
+          content: formatMoney(ns, nodeStats.production) + ' / s',
+        },
+        {
+          column: { name: 'Level', style: { textAlign: 'center' } },
+          content: nodeStats.level,
+        },
+        {
+          column: { name: 'RAM', style: { textAlign: 'center' } },
+          content: ns.formatRam(nodeStats.ram),
+        },
+        {
+          column: { name: 'Cores', style: { textAlign: 'center' } },
+          content: nodeStats.cores,
+        },
+      ],
+    };
+    table.rows.push(row);
+  }
+
+  // Total row.
+  table.rows.push({
+    cells: [
+      { column: NODE_NAME_COLUMN, content: 'TOTAL' },
+      {
+        column: PRODUCTION_COLUMN,
+        content: formatMoney(ns, totalProduction) + ' / s',
+      },
+    ],
+    style: {
+      color: ns.ui.getTheme().success,
+    },
+  });
+
+  printTable(ns, table);
 }
